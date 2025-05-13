35d093454acf03f61f0da0c0ad8ece578d69b133
Ebrahim <elsayedebrahim603@gmail.com>
2025-05-13 16:34:59 +0300

    feat: initialize NestJS application with basic structure and logger middleware
    - Added package.json for project dependencies and scripts.
    - Implemented RequestLogger middleware for logging requests.
    - Set up AppModule to include middleware and controller.
    - Added main.ts to bootstrap the application.
    - Created unit tests for AppController.
    - Developed end-to-end tests for the application.
    - Configured Jest for testing with e2e specifications.
    - Added TypeScript configuration files for building and development.

---
+++ src/Modules/app/app.module.ts
@@ -0,0 +1,15 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RequestLogger } from 'src/Common/middlewares/logger.middleware';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLogger).forRoutes('songs');
+  }
+}
